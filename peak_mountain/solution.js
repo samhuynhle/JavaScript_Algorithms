42a5476b9175c8fe807bf23b1b756f403abee891
samhuynhle <samhuynhle@gmail.com>
2019-11-18 13:02:57 -0800

    comment left on peak_mountain

--- peak_mountain/solution.js
+++ peak_mountain/solution.js
@@ -1,6 +1,6 @@
-const peakIndexInMountainArray = A => {
-    const prop = x => {
-        if(A[x] > A[x + 1]) {
+const peakIndexInMountainArray = A => { // Take in array
+    const prop = x => { // establish a function to pass into helper function
+        if(A[x] > A[x + 1]) { // if value at index x is less then value at index + 1 is the condition, then return true
             return true;
         }
     }
@@ -8,18 +8,18 @@ const peakIndexInMountainArray = A => {
 };
 
 const lowerBound = (arr, prop) => {
-    var left = 0;
-    var right = arr.length - 1;
-    while (left <= right){
-        var mid = parseInt((left + right) / 2);
-        if(prop(mid)){
+    var left = 0; // set left index
+    var right = arr.length - 1; // set right index
+    while (left <= right){ // while loop condition: while left is less than or equal to right continue
+        var mid = parseInt((left + right) / 2); // we establish a mid point every iteration
+        if(prop(mid)){ // if the value at mid is less than the value to the right
             console.log("right", right);
-           right = mid - 1;
+           right = mid - 1; // we move the right index to the value left of mid
         } else {
-            left = mid + 1;
+            left = mid + 1; // otherwhise we move the left value to the right of mid
        }
-    }
-    return left;
+    } // our loop will continue, either moving the right or left index
+    return left; // we return the left value, which is where the peak of the mountain is
 }
 
 const upperBound = (arr, prop) => {
