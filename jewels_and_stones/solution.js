d8848b5292c3b717a90bd285293d3e6845a30d9e
samhuynhle <samhuynhle@gmail.com>
2019-11-18 16:14:33 -0800

    added comments to jewels

--- jewels_and_stones/solution.js
+++ jewels_and_stones/solution.js
@@ -16,14 +16,14 @@ var numJewelsInStones = function(J, S) {
 
 // Optimized with set
 
-var numJewelsInStones = function(J, S) {
+var numJewelsInStones = function(J, S) { // Takes in two strings J and S, which we will review their characters
 
-    if(S.length==="" && J.length==="" ) return 0;
-    if(S.length===0 || J.length===0) return 0;
+    if(S.length===0 && J.length===0 ) return 0; // conditionals for quick sets, if both have length 0, return 0
+    if(S.length===0 || J.length===0) return 0; // if either have length 0, return 0, since if there's a zero that means no jems
 
-    var count = 0;
-    const set1 = new Set(J.split());
-    for(var schar of S){
+    var count = 0; // initialize a count
+    const set1 = new Set(J.split()); // Create a new set that contains J
+    for(var schar of S){ // sift through each character of s and see if it matches any of the characters in J
         if(set1.has(schar)){
             count ++;
         }
