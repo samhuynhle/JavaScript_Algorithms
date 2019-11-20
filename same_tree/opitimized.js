3bdd22029cb6954597c2d107fb00d925f33f0be9
samhuynhle <samhuynhle@gmail.com>
2019-11-20 12:05:41 -0800

    added comments to same tree match

--- same_tree/opitimized.js
+++ same_tree/opitimized.js
@@ -1,15 +1,17 @@
-var isSameTree = function(p, q){
-    if(p == null && q == null){
+var isSameTree = function(p, q){ // this function takes two nodes to check if they are the same tree
+    if(p == null && q == null){ // if both nodes are null, you are at a leaf node
         return true
     }
-    if(p == null && q){
+    if(p == null && q){ // if one of the nodes are null but the other exist during one of the recursive calls or first one, then they are not same tree
         return false;
     }
-    if(p && q == null){
+    if(p && q == null){ // another false throw, same as above but for other node
         return false;
     }
-    if(p.val != q.val){
+    if(p.val != q.val){ // another false throw, if they don't match
         return false;
     }
-    return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+    return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right)); // We are going to return true when we reach this and the first condition our true base case is met
 };
+
+// We are going through each node of the tree's together comparing current, then left and then right branches
