dabae712fed26066136f810402cf58192ff5d312
samhuynhle <samhuynhle@gmail.com>
2019-11-20 12:37:44 -0800

    added optimized solution to search_in_BST

--- search_in_BST/solution.js
+++ search_in_BST/solution.js
@@ -33,3 +33,20 @@ var searchBST = function(root, val) {
     return null;
 };
 
+// Optimized solution for BST's
+
+var searchBST = function(root, val) {
+    if(root == null){
+        return null;
+    }
+    if(root.val == val){
+        return root;
+    }
+    if(root.val > val){
+        return searchBST(root.left, val);
+    }
+    else {
+        return searchBST(root.right, val);
+    }
+    return new TreeNode(null);
+}
