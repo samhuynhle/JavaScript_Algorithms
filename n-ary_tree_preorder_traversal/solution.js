0de85f1b056a8bb18f478d3acd732d09e8fbb8fe
samhuynhle <samhuynhle@gmail.com>
2019-11-21 13:49:57 -0800

    iterative solution using stack for preorder traversal added

--- n-ary_tree_preorder_traversal/solution.js
+++ n-ary_tree_preorder_traversal/solution.js
@@ -9,6 +9,8 @@
  * @param {Node} root
  * @return {number[]}
  */
+
+ //Recursive Solution
 var preorder = function(root) { // Main function to create the return array and call the initial function of the recursive function
     var ret = []; // We will store the values of the nodes where and return this list
     traversal(root, ret); // Pass into the traversal function the tree (root node) and the array
@@ -26,3 +28,20 @@ var traversal = function(node, array) { // Takes in any Node and array
     }
     return array;
 }
+
+// Iterative Solution
+var preorder2 = function(root) {
+    var curr = root;
+    var stack = []; // This is the stack we will use to ensure we have visited every node
+    var result = []; // This is our return
+
+    while(curr != null){
+        result.push(curr.val);
+        // for the current node we will look at the children. We need to reverse the children before we insert into the stack because the first child
+        // left most will need to be added into our stack last for each node we visit. We then add onto the stack the left most children's children
+        curr.children.reverse().map(ele => stack.push(ele));
+        curr = stack.pop(); // Then we take the top most and assgn curr to it for the next iteration
+    }
+
+    return result;
+};
