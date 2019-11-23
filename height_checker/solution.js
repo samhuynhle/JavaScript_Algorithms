73bd3e8c314259c4cd738efe9f9d214a241b0f7b
samhuynhle <samhuynhle@gmail.com>
2019-11-22 18:33:56 -0800

    left comments in solution

--- height_checker/solution.js
+++ height_checker/solution.js
@@ -2,19 +2,19 @@
  * @param {number[]} heights
  * @return {number}
  */
-var heightChecker = function(heights) {
-    var errors = 0;
-    var sorted = sorting(heights);
-    console.log(heights);
-    console.log(sorted);
-    for(let i = 0; i < heights.length; i++){
+
+// Unpptimized solution, we are using builting sort function in Array class for javascript.
+var heightChecker = function(heights) { // main function
+    var errors = 0; // establish error counter
+    var sorted = sorting(heights); // creating a new array that is the original array sorted
+    for(let i = 0; i < heights.length; i++){ // loop through both arrays and compare
         if(sorted[i] != heights[i]){
             errors++;
         }
     }
-    return errors;
+    return errors; // return number of errors
 };
 
-var sorting = function(array) {
-    return array.slice(0).sort(function(a, b){return a-b});
+var sorting = function(array) { // helper function to sort the array
+    return array.slice(0).sort(function(a, b){return a-b}); // needed to splice as the .sort method changes in place, also needed the modify the compare function wihtin sort method to account for numbers
 }
