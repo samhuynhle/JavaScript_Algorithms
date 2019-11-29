1b1b5c5a4c57342651f1b615be47ed67b6d43c5b
Sam <samhuynhle@gmail.com>
2019-11-29 03:13:08 -0800

    added comments to the optimized solution to add two numbers linked list algorithm

--- add_two_numbers_linked_list/solution.js
+++ add_two_numbers_linked_list/solution.js
@@ -11,18 +11,18 @@ function ListNode(val) {
 }
 
 var addTwoNumbers = function(l1, l2) {
-    let dummyHead = new ListNode(0)
-    let curr = dummyHead
-    let carry = 0
-    while(l1 != null || l2 != null) {
-        sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0)
-        carry = Math.floor(sum / 10)
-        curr.next = new ListNode(sum % 10)
-        curr = curr.next
-        l1 = (l1 ? l1.next : l1)
-        l2 = (l2 ? l2.next : l2)
+    let dummyHead = new ListNode(0) // We are establishing a dummy head so we can return the next
+    let curr = dummyHead // we will establish a current runner to keep adding to our list
+    let carry = 0 // need to be able to store carry value
+    while(l1 != null || l2 != null) { // our conditional is if we have any values in l1 or l2
+        sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0) // our sum will be equal to the carrry plus l1 or l2 value if they exist
+        carry = Math.floor(sum / 10) // the carry will be 1 if the sum is > 10, will be 0 if less than 10
+        curr.next = new ListNode(sum % 10) // adding the sum, will always reduce to single digit, the carry is already stored if needed
+        curr = curr.next // advance our curr runner
+        l1 = (l1 ? l1.next : l1) // if l1 is valid then move to the next
+        l2 = (l2 ? l2.next : l2) // if l2 is valid then move to the next
     }
-    if(carry > 0) {
+    if(carry > 0) { // if we have a carry, add to the end
         curr.next = new ListNode(carry)
     }
     return dummyHead.next
