183e3d0f7058cdd2219bb65f7aaa57157be2ba56
Sam <samhuynhle@gmail.com>
2019-11-30 00:54:00 -0800

    completed lps algorithm, need to do walk throughs for more DP practice

--- longest_palindromic_substring/solution.js
+++ longest_palindromic_substring/solution.js
@@ -36,7 +36,7 @@ var longestPalindrome = function(s) {
     // i, j denotes start and edn index of the given string s.
     // We are creating a new instance of an Array that will be able to store ranges i, j
     // Initially, this will all be false.
-    const dp = [...new Array(s.length +1)].map(_ => new Array(s.length + 1).fill(false));
+    const dp = [...new Array(s.length + 1)].map(_ => new Array(s.length + 1).fill(false));
     let lps = '';
 
     // This will be the base case for one character, we are getting sure we have this
@@ -48,24 +48,21 @@ var longestPalindrome = function(s) {
 
     // This will be for two characters
     for(let i = 0; i < s.length; i++) {
+        // this is checking if the current range is a palindrome
+        if(s[i] === s[i + 1]) dp[i][i+1] = true;
+
+        // if it is a valid palindrome we compare it to the last lps recorded and lps will be come which ever is longer
+        if(dp[i][i+1]) lps = s.substring(i, i + 2);
+    }
+
+    for(let i = s.length - 1; i >= 0; i--) {
         for(let j = i + 2; j < s.length; j++) {
 
             // this is checking if the current range is a palindrome
-            if(s[i] === s[i + 1]) dp[i][i+1] = true;
-
-            // if it is a valid palindrome we compare it to the last lps recorded and lps will be come which ever is longer
-            if(dp[i][i+1]) lps = s.substring(i, i + 2);
-        }
-    }
-
-    for(let i = s.length -1; i >= 0; i--) {
-        for(let j = i +2; j < s.length; j++) {
-
-            // this is checking if the current range is a palindrome
-            dp[i][j] = dp[i+1][j-1] && s[i] == s[j];
+            dp[i][j] = dp[i+1][j-1] && s[i] === s[j];
 
             // doing a comparison for lps
-            if(dp[i][j]) lps = lps.length < (j - 1 + 1) ? s.substring(i, j + 1) : lps;
+            if(dp[i][j]) lps = lps.length < (j - i + 1) ? s.substring(i, j + 1) : lps;
         }
     }
     return lps;
