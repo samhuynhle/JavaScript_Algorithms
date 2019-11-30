d21e3f11c79534babc69fdc994d06c0769ebe54d
Sam <samhuynhle@gmail.com>
2019-11-29 21:47:08 -0800

    added comments to solution for longest_substring_without_repeating_characters algorithm

--- longest_substring_without_repeating_characters/solution.js
+++ longest_substring_without_repeating_characters/solution.js
@@ -15,7 +15,22 @@
  // 2. If it is, then our current NRCS changes. It becomes the substring starting from the
  // next character of the previous instance to the currently scanned character.
  // Then we compare the current length to the max length before chaning the current NCRS or changing current length
-var lengthOfLongestSubstring = function(s) {
-    var n = s.length;
-    
+
+ var lengthOfLongestSubstring = function(s) {
+    const length = s.length; // Store the length of the string passed in
+    let windowStart = 0; // windowStart is the beginning of each substring
+    let current = 0; // current is the current letter we are at
+    let longest = 0; // longest is the longest substring we have
+    let found = new Map(); // we are using JavaScript's hash map to store the characters and the index they are at
+
+    while (current < length) {
+        const c = s[current]; // set c to the current character we are at
+        if (found.get(c) >= windowStart ) { // if the character is in the map and it's position is greater than where our window is
+            windowStart = found.get(c) + 1; // then we move the window to that index plus one, we need to move the window as there will be a duplicate
+        }
+        found.set(c, current); // If the character is not in the map, then we add it along with it's index
+        longest = Math.max(longest, (current - windowStart) + 1); // then we take the greater of the two, the past longest or the difference between current and start (plus one since we index at 0)
+        current++; // we advance our current runner until the end
+    }
+    return longest; // we then return the longest
 };
