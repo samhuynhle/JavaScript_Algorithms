652d792848d16f75af318b0b56015dc6c405f333
Sam <samhuynhle@gmail.com>
2019-12-09 23:56:40 -0800

    more optimized solution added with comments

--- valid_anagram/solution.js
+++ valid_anagram/solution.js
@@ -27,3 +27,28 @@ var isAnagram = function(s, t) {
     }
     return true;
 };
+
+/*
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+ // optimized solution using an array using the indexes matching character codes and storing counts. Faster look up time.
+ // for letter in s we increase and for letter in t we decrease, if we find anything > 0 in our second loop we return false
+var isAnagram2 = function(s, t) {
+    let map = Array(256).fill(0)
+    if(s.length!=t.length) {
+        return false
+    }
+    for(let i =0; i<s.length;i++) {
+        map[s.charCodeAt(i)]++;
+        map[t.charCodeAt(i)]--;
+    }
+    for(let i =0; i<256;i++) {
+        if(map[i]) {
+            return false
+        }
+    }
+    return true
+};
