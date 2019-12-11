a09727f1e1d1849984f2c939cd774f0b2a6174e7
samhuynhle <samhuynhle@gmail.com>
2019-12-10 18:45:16 -0800

    added comments to find_positive_integer_solution algorithm

--- find_pos_integer_solution/solution.js
+++ find_pos_integer_solution/solution.js
@@ -11,18 +11,18 @@
  *
  * };
  */
-/**
+/*
  * @param {CustomFunction} customfunction
  * @param {integer} z
  * @return {integer[][]}
  */
-var findSolution = function(customfunction, z) {
-    const equation = customfunction.f;
-    let result = [];
-    for (let i = 1; i <= z; i++) {
-        for (let j = 1; j <= z; j++){
+var findSolution = function(customfunction, z) { // takes in function and integer value z
+    const equation = customfunction.f; // we establish the function so we can call it in our loop
+    let result = []; // we store the results in an array that will hold arrays storing the x and y integers
+    for (let i = 1; i <= z; i++) { // all results will be positive as stated in problem description
+        for (let j = 1; j <= z; j++){ // need to do both
             if (equation(i,j) === z){
-                result.push([i, j]);
+                result.push([i, j]); // if the result is equal to the z integer, push the pairing
             }
         }
     }
