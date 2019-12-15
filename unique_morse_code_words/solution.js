61a50c40388d985503bd146230f567f9b948cf62
samhuynhle <samhuynhle@gmail.com>
2019-12-15 02:56:52 -0800

    added comments to unique morse code words algorithm

--- unique_morse_code_words/solution.js
+++ unique_morse_code_words/solution.js
@@ -28,15 +28,15 @@ var morseMap = {
 };
 
 var uniqueMorseRepresentations = function(words) {
-    const set1 = new Set();
-    for(var word of words){
-        var replace = '';
-        for(var char of word){
+    const set1 = new Set(); // we will be using a Set object to keep track of the converted words (morse codes), sets only store unique 'keys'
+    for(var word of words){ // loop over each word in the words array
+        var replace = ''; // set up a new string
+        for(var char of word){ // within each word, we look at each character and add to our string the equivalent morse code
             replace += morseMap[char];
         }
-        if(!set1.has(replace)){
-            set1.add(replace);
+        if(!set1.has(replace)){ // we check if the set has that morse coded word
+            set1.add(replace); // add it if it doesn't
         }
     }
-    return set1.size;
+    return set1.size; // return the set size
 };
