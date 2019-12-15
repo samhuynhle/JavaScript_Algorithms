ba08b5ecc03da9ebf39357dd671db5343b456e26
samhuynhle <samhuynhle@gmail.com>
2019-12-15 03:08:45 -0800

    reviewed and added comments to subdomain visit count algorithm, will need to review once more to get used to string splits and callback functions

--- subdomain_visit_count/solution.js
+++ subdomain_visit_count/solution.js
@@ -1,11 +1,13 @@
 var subdomainVisits = function(cpdomains) {
-    const domCounts = new Map();
+    const domCounts = new Map(); // the map will store the domains (the different levels) and the counts they have
     
     // utlizing a callback functino for each item that is in the cpdomains, forEach method allows use to use callback function on each item
     cpdomains.forEach(pair => {
       [count, domain] = pair.split(' '); // each item is a count and the domain that are seperated with ' '; the count is the first part and domain second part
-      if (!domCounts[domain]) domCounts[domain] = 0; // if the counts do not contain the domain then we add teh domain into the the map
-      domCounts[domain] += parseInt(count); // if it was already in the hash map, then we increment the count, but we still increment the count by one
+      if (!domCounts[domain]){ // if the counts do not contain the domain then we initialize it into the the map
+        domCounts[domain] = 0;
+      }
+      domCounts[domain] += parseInt(count); // we then increment the domain counts by the values stored in the variable count
       
       while (domain.length > 0) { // we are now looking at the domain itself to find other sub domains
         const index = domain.indexOf('.'); // we split the domain on the '.' and store the index
