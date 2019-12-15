482b30ab80cd6a1cad19e6dacf686baad4599cb2
samhuynhle <samhuynhle@gmail.com>
2019-12-15 03:23:55 -0800

    reviewed split a balance string algorithm, added comments

--- split_a_balanceed_string/solution.js
+++ split_a_balanceed_string/solution.js
@@ -1,13 +1,13 @@
-var balancedStringSplit = function(s) {
+var balancedStringSplit = function(s) { // function takes in a string of L's and R's
     count = 0;
-    balance = 0;
+    balance = 0; // this will be we know we found the balance equal amounts of L's and R's in relation to each other (similar to valid parens)
     for(var schar of s){
         if(schar == 'L'){
             balance++;
         } else {
             balance--;
         }
-        if(balance == 0){
+        if(balance == 0){ // every time we hit a zero, we know we found a balance sequence of L's and R's
             count ++;
         }
     }
