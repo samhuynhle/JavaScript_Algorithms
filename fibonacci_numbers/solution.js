a241697577e1dbe159150f1d3d4ae66cb2387fe2
Sam Le <samhuynhle@gmail.com>
2019-12-19 17:51:09 -0800

    added comments to fibonacci algorithm

--- fibonacci_numbers/solution.js
+++ fibonacci_numbers/solution.js
@@ -1,7 +1,11 @@
 /*
  * @param {number} N
  * @return {number}
- */
+ *
+*/
+
+// fibonacci is a sequence where current is equal to current - 1 plus current - 2 within the sequesequenc
+// we use recursive function to call on our function as needed, with base cases.
 var fib = function(N) {
     if(N == 0){
         return 0;
