e272d100ac4207540d8e736964d893e5bb1bfa47
Sam <samhuynhle@gmail.com>
2019-12-20 21:34:57 -0800

    search insert position completed, started count and say algorithm

--- search_insert_position/solution.js
+++ search_insert_position/solution.js
@@ -3,12 +3,27 @@
  * @param {number} target
  * @return {number}
  */
+
+// var searchInsert = function(nums, target) {
+//     let index = 0;
+
+//     while(nums[index] < target){
+//             index++;
+//     }
+
+//     return index;
+// };
+
+// Binary Search
 var searchInsert = function(nums, target) {
-    let index = 0;
-
-    while(nums[index] < target){
-            index++;
+    var left = 0, right = nums.length;
+    while(left < right) {
+        var middle = left + Math.floor((right - left)/2);
+        if(target > nums[middle]){
+            left = middle + 1;
+        } else {
+            right = middle;
+        }
     }
-
-    return index;
-};
+    return left;
+}
