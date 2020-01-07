5e69c696919c646666208967be4e71f7f6cd08c7
Sam <samhuynhle@gmail.com>
2020-01-07 01:20:09 -0800

    reviewed maximum_subarray algorithm. Main concept, looping through the string keeping track of previous sum and current maximums using JavaScript's max method

--- maximum_subarray/solutoin.js
+++ maximum_subarray/solutoin.js
@@ -1,13 +1,18 @@
 /*
- * @param {number[]} nums
- * @return {number}
- */
+@param {number[]} nums
+@return {number}
+
+*/
+
 var maxSubArray = function(nums) {
     var previous = 0;
     var max = -Number.MAX_VALUE;
 
     for(var i = 0; i < nums.length; i++){
+        // Here, we are setting previous either to the previous plus our current number or just the current number, which ever is greater
         previous = Math.max(previous + nums[i], nums[i]);
+
+        // Here, we then determine if we have a new maximum based on the past maximum and the sum of the previous numbers or new previous sum
         max = Math.max(max, previous);
     }
     return max;
