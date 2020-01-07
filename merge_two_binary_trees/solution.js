e0ae0fe524116e0dca9c68193522efb7f124f867
Sam <samhuynhle@gmail.com>
2020-01-07 01:24:17 -0800

    reviewed merge_two_binary_tree algorithm. Main concept. traversing through binary tree with recursion

--- merge_two_binary_trees/solution.js
+++ merge_two_binary_trees/solution.js
@@ -6,10 +6,12 @@ function TreeNode(val) {
 }
 
 var mergeTrees = function(t1, t2) { // we will be merging t2 into t1 and return t1
-    if (!t1 && !t2) return null; // Base Case --> If both nodes are null return null
-    if (!t1 || !t2) return t1 || t2; // Base Case 2 --> If one of the nodes are null, return the not null
+    if (!t1 && !t2) return null; // Base Case --> If both nodes are null return null, there's nothing to add
+    if (!t1 || !t2) return t1 || t2; // Base Case 2 --> If one of the nodes are null, return the not null node, will add as child
     
-    t1.val += t2.val; // Base Case 3 --> If both nodes are not null, sum the node values
+    t1.val += t2.val; // Base Case 3 --> If both nodes present, we add the values and store in t1
+
+    // Recursive call for the children on the left, then children on the right. We'll end up going to left most child first
     t1.left = mergeTrees(t1.left, t2.left); // recursive call for left children of the two trees
     t1.right = mergeTrees(t1.right, t2.right); // recursive call for right children of the two trees
     
