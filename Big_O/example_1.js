83dd5d342271525fa2def8748cb17359390019e5
Sam <samhuynhle@gmail.com>
2020-01-28 19:14:17 -0800

    Big O note

--- Big_O/example_1.js
+++ Big_O/example_1.js
@@ -1,14 +1,11 @@
 const nemo = ['nemo'];
 
 const findNemo = (array) => {
-    let time0 = performance.now();
     for (let i = 0; i < array.length; i++){
         if(array[i] === 'nemo') {
             console.log('Found NEMO!');
         }
     }
-    let time1 = performance.now();
-    console.log('Call to find Nemo took: ' + (time1 - time0) + ' milliseconds');
 }
 
 findNemo(nemo);
---
+++ Big_O/example_2.js
@@ -0,0 +1,7 @@
+const boxes = [1,2,3,4,5,6];
+
+const compressFirstBox = (boxes) => {
+    console.group(boxes[0]);
+}
+
+// this is O(1), constant time
